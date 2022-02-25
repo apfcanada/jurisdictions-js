a28041af17ac0fcb41051d752affb6ee7c5c0f10
Nate Wessel <bike756@gmail.com>
2022-02-25 15:46:47 -0500

    use proper private fields

--- src/connection.js
+++ src/connection.js
@@ -3,6 +3,7 @@ import { Jurisdiction } from './jurisdiction.js'
 // this is a highly abstract class for undirected connections between two or 
 // more jurisdictions
 export class Connection {
+	#jurs
 	constructor(...jurs){
 		// validate inputs
 		if( jurs.length < 2 ){
@@ -12,24 +13,26 @@ export class Connection {
 		}else if( jurs.length != (new Set(jurs)).size ){
 			return console.warn('connections cannot be reflexive')
 		}
-		this._jurs = jurs
+		this.#jurs = jurs
 	}
 	get id(){ // unique to this set of jurisdictions
-		return this._jurs.map(j=>j.geo_id).sort((a,b)=>b-a).join('|')
+		return this.#jurs.map(j=>j.geo_id).sort((a,b)=>b-a).join('|')
 	}
 }
 
 // for directed connections between two jurisdictions
 export class DirectedConnection extends Connection{
+	#to
+	#from
 	constructor(source,target){
 		super(source,target)
-		this._from = source
-		this._to = target
+		this.#from = source
+		this.#to = target
 	}
 	get from(){
-		return this._from
+		return this.#from
 	}
 	get to(){
-		return this._to
+		return this.#to
 	}
 }
