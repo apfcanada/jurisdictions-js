a8689d27290d1a05eda39199af61122d2de78c5a
Nate Wessel <bike756@gmail.com>
2022-02-25 16:55:45 -0500

    more connection tests

--- tests/connections.test.js
+++ tests/connections.test.js
@@ -1,18 +1,28 @@
 import { 
 	JurisdictionGraph,
+	Connection,
 	DirectedConnection
 } from '../src/'
 import staticData from './staticGraphData.json'
 
-test('Can make and validate connections',() => {
+test('Can create and validate general connections',() => {
 	const graph = new JurisdictionGraph(staticData);
-	const [toronto,beijing] = graph.lookupNow([10,111])
-	const [ottawa,shanghai] = graph.lookupNow([21,240])
-	const connections = [
-		new DirectedConnection(toronto,beijing),
-		new DirectedConnection(ottawa,shanghai)
-	]
-	// these are invalid connections
+	const [toronto,beijing,ottawa,shanghai] = graph.lookupNow([10,111,21,240])
+	// valid
+	expect(()=>new Connection(toronto,beijing,ottawa,shanghai)).not.toThrow()
+	// invalid
+	expect(()=>new Connection(toronto,toronto,shanghai)).toThrow()
+	expect(()=>new Connection(beijing)).toThrow()
+} )
+
+test('Can create and validate directed connections',() => {
+	const graph = new JurisdictionGraph(staticData);
+	const [toronto,beijing,ottawa,shanghai] = graph.lookupNow([10,111,21,240])
+	// valid
+	expect(()=>new DirectedConnection(toronto,beijing)).not.toThrow()
+	expect(()=>new DirectedConnection(ottawa,shanghai)).not.toThrow()
+	// invalid
 	expect(()=>new DirectedConnection(toronto,toronto)).toThrow()
 	expect(()=>new DirectedConnection(toronto,undefined)).toThrow()
+	expect(()=>new DirectedConnection(toronto)).toThrow()
 } )
