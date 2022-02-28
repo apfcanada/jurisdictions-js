7d4ad16cd194bf848e6d57f5513db8d986643fe1
Nate Wessel <bike756@gmail.com>
2022-02-28 14:20:08 -0500

    DRY out tests

--- tests/connections.test.js
+++ tests/connections.test.js
@@ -6,9 +6,11 @@ import {
 } from '../src/'
 import staticData from './staticGraphData.json'
 
+const graph = new JurisdictionGraph(staticData);
+const [toronto,beijing,ottawa,shanghai] = graph.lookupNow([10,111,21,240])
+const [canada,china] = graph.lookupNow([2,3])
+
 test('Can create and validate general connections',() => {
-	const graph = new JurisdictionGraph(staticData);
-	const [toronto,beijing,ottawa,shanghai] = graph.lookupNow([10,111,21,240])
 	// valid
 	expect(()=>new Connection(toronto,beijing,ottawa,shanghai)).not.toThrow()
 	// invalid
@@ -17,8 +19,6 @@ test('Can create and validate general connections',() => {
 } )
 
 test('Can create and validate directed connections',() => {
-	const graph = new JurisdictionGraph(staticData);
-	const [toronto,beijing,ottawa,shanghai] = graph.lookupNow([10,111,21,240])
 	// valid
 	expect(()=>new DirectedConnection(toronto,beijing)).not.toThrow()
 	expect(()=>new DirectedConnection(ottawa,shanghai)).not.toThrow()
@@ -29,9 +29,6 @@ test('Can create and validate directed connections',() => {
 } )
 
 test('Can create a connection aggregator',() => {
-	const graph = new JurisdictionGraph(staticData);
-	const [toronto,beijing,ottawa,shanghai] = graph.lookupNow([10,111,21,240])
-	const [canada,china] = graph.lookupNow([2,3])
 	const conns = [
 		new Connection(toronto,beijing),
 		new DirectedConnection(toronto,shanghai),
@@ -43,9 +40,6 @@ test('Can create a connection aggregator',() => {
 } )
 
 test('Can aggregate to countries',() => {
-	const graph = new JurisdictionGraph(staticData);
-	const [toronto,beijing,ottawa,shanghai] = graph.lookupNow([10,111,21,240])
-	const [canada,china] = graph.lookupNow([2,3])
 	const conns = [
 		new Connection(toronto,beijing),
 		new DirectedConnection(toronto,shanghai),
@@ -61,9 +55,6 @@ test('Can aggregate to countries',() => {
 } )
 
 test('Can aggregate with focus',() => {
-	const graph = new JurisdictionGraph(staticData);
-	const [toronto,beijing,ottawa,shanghai] = graph.lookupNow([10,111,21,240])
-	const [canada,china] = graph.lookupNow([2,3])
 	const conns = [
 		new Connection(toronto,beijing),
 		new DirectedConnection(toronto,shanghai),
